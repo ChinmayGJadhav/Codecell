ff471f1c115a079e117e5186bddd65e4cf43d45a
ChinmayGJadhav <51798073+ChinmayGJadhav@users.noreply.github.com>
2019-08-26 13:29:03 +0530

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -5,20 +5,14 @@ Created on Tue Aug 13 22:51:30 2019
 @author: chinmay
 """
 
-
-c=3
-L=list()
-ans=0
-final_ans=0
-while c!=0: 
-    T=int(input())
-    L=input().split(" ")
-    for i in range(T):
-        L[i]=int(L[i])
-    for i in range(T):
-        for j in range(i+1,T):
-            ans+= L[i]&L[j]
-    c-=1 
-    final_ans+=ans%101
-    ans=0
-print(final_ans)     
+x,y,z=input().split(" ")
+x=int(x)
+y=int(y)
+z=int(z)
+if x<y:
+    ans=x-z
+else:
+    for i in range(2,z+1):
+        x-=2
+    ans=x
+print(ans)
